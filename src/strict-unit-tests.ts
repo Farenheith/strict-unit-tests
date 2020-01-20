6f55095aa26437868355b5159ffd13d88017a3ea
Thiago Oliveira Santos <thiago.santos@maxmilhas.com.br>
2020-01-20 08:38:03 -0300

    BeforeEach restore

--- src/strict-unit-tests.ts
+++ src/strict-unit-tests.ts
@@ -8,13 +8,16 @@ export { before, after, beforeEach, afterEach, describe, it } from 'mocha';
 export { stubSuperConstructor } from 'sinon-chai-calls-assertion';
 export * from 'strict-mocha-describers';
 
+import { beforeEach } from 'mocha';
+
 export function stub<T, K extends keyof T>(obj?: T, method?: K) {
 	return sinon.stub(obj!, method!) as sinon.SinonStub;
 }
+import 'mocha';
 
 chai.use(callsLike);
 enforceStubsAssertions(sinon, chai);
 
 beforeEach(() => {
 	sinon.restore();
-})
+});
